103aeb696c799cb9252ad12a755513106f0576a2
Code-wizard-satyam <satyamkumarcr07@gmail.com>
2025-07-29 13:30:31 +0530

    bbl short improvement

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,15 @@
 def bubble_sort(arr):
     size = len(arr)
 
-    for passes in range(size):
-        for j in range(0, size-1):
+    for passes in range(size-1):
+        end = size - passes -1
+        for j in range(0, end):
             if (arr[j] > arr[j+1]):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
-unordered_list = [24,87,97,45,67,98,22,24,56]
+unordered_list = [8,6,9,5,2]
 ordered_list = bubble_sort(unordered_list)
 
 print("Our shorted list is ", ordered_list)

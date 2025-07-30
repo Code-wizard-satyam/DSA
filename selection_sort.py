c445e56f7cc9908d1cfdbf485b1e14e5128ca3e3
Code-wizard-satyam <satyamkumarcr07@gmail.com>
2025-07-30 08:16:06 +0530

    corrections

--- selection_sort.py
+++ selection_sort.py
@@ -1,19 +1,14 @@
 def selection_sort(arr):
-    size =len(arr)
-    min_index = 0
-    start = 0
+    n =len(arr)
+    for i in range(n-1):
+        min_index = i
 
-    for passes in range(size):
-        print("Pass: ", passes)
-        for j in range(start, size-1):
-            if arr[j] < arr[min_index]:
+        for j in range(i+1,n):
+            if (arr[j] < arr[min_index]):
                 min_index = j
-                print("Minimun Number:", arr[min_index])
 
-            arr[start], arr[min_index] = arr[min_index], arr[start]
-            print("Arr: ", arr)
-        start += (passes + 1)
-
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
 
 unordered_list = [4,2,3,1,5,7,6]
 result = selection_sort(unordered_list)
